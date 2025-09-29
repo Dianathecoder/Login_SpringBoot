01410318b5e663aca1501e2cae51730a592fa443
Dianathecoder <diana.canyas15@gmail.com>
2025-09-29 12:17:10 +0200

    init commit

---
+++ src/main/java/com/example/Login_RA_02/LoginRa02Application.java
@@ -0,0 +1,13 @@
+package com.example.Login_RA_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRa02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRa02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Login_RA_02/LoginRa02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Login_RA_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRa02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
